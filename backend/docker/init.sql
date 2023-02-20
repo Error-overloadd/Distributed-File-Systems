237f50820aaadf3ab2a33b534ee01cd9f7ab4a94
Abid-Al-Labib <getabidallabib@gmail.com>
2023-02-20 13:44:14 -0700

    setup for db docker complete

---
+++ backend/docker/init.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS fileDB;
+USE fileDB;
+
+CREATE TABLE dds_file_server (
+    id bigint primary key,
+    name varchar(255),
+    address varchar(255),
+    type varchar(255),
+    dds_store_path varchar(255),
+    dds_bin_path varchar(255)
+);
+
+CREATE TABLE dds_file (
+    fileId bigint primary key,
+    name varchar(255),
+    size bigint,
+    content_type varchar(255),
+    created_date DateTime DEFAULT CURRENT_TIMESTAMP,
+    fileserver bigint references dds_file_server(id),
+    path varchar(255)
+);
