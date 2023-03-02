76db25efffc2d9c2b64509879c44e2deea76ecc7
Abid-Al-Labib <getabidallabib@gmail.com>
2023-03-02 12:12:59 -0700

    user db added

--- backend/docker/init/init.sql
+++ backend/docker/init/init.sql
@@ -1,22 +1,33 @@
 CREATE DATABASE IF NOT EXISTS fileDB;
+CREATE DATABASE IF NOT EXISTS userDB;
 USE fileDB;
 
 CREATE TABLE dds_file_server (
-    id bigint primary key AUTO_INCREMENT,
-    name varchar(255),
-    address varchar(255),
-    type varchar(255),
-    dds_store_path varchar(255),
-    dds_bin_path varchar(255)
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    address VARCHAR(255),
+    type VARCHAR(255),
+    dds_store_path VARCHAR(255),
+    dds_bin_path VARCHAR(255)
 );
 
 CREATE TABLE dds_file (
-    fileId bigint primary key AUTO_INCREMENT,
-    name varchar(255),
-    size bigint,
-    content_type varchar(255),
-    created_date DateTime DEFAULT CURRENT_TIMESTAMP,
-    fileserver bigint references dds_file_server(id),
-    path varchar(255),
-    user varchar(255)
+    fileId BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    size BIGINT,
+    content_type VARCHAR(255),
+    created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    fileserver BIGINT REFERENCES dds_file_server(id),
+    path VARCHAR(255),
+    user VARCHAR(255)
+);
+
+USE userDB;
+
+CREATE TABLE user (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    isAdmin BOOLEAN
 );
