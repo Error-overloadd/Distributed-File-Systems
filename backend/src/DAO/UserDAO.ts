653df078d48d0b3c22a241ea98bfcbf9e1fa8fd1
PearlBorromeo <lotuspearl.borromeo@ucalgary.ca>
2023-03-03 04:51:01 -0700

    uniquely randomized user id

--- backend/src/DAO/UserDAO.ts
+++ backend/src/DAO/UserDAO.ts
@@ -32,7 +32,13 @@ export class UserDAO{
 
     // sql query to add user to database for registration
     addUser(user: any, callback: Function){
-        this.dbConnection.query(`INSERT INTO user(id, name, email, password, isAdmin) VALUES(${user.id},'${user.name}','${user.email}','${user.password}',${user.isAdmin});`, (err: any,rows: any) => {
+        const dateStr:any = Date.now().toString(36); // convert num to base 36 and stringify
+        const randomStr:any = Math.random().toString(36).substring(2, 8); // start at index 2 to skip decimal point
+        const id = `${dateStr}-${randomStr}`;
+        
+        console.log("ID:"+id);
+
+        this.dbConnection.query(`INSERT INTO user(id, name, email, password, isAdmin) VALUES('${id}','${user.name}','${user.email}','${user.password}',${user.isAdmin});`, (err: any,rows: any) => {
             if(err) throw err;
           
             console.log('Data received from Db:');
@@ -43,6 +49,7 @@ export class UserDAO{
 
     // sql query to retrieve user from database for login
     getUser(email:string, callback: Function){
+
         const q = `SELECT * FROM user WHERE email = '${email}'`;
         console.log(q);
         this.dbConnection.query(`SELECT * FROM user WHERE email = '${email}'`, (err: any,rows: any) => {
