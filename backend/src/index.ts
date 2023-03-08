db7602616d231fba659e8b79bbb6f56062ff493d
hrithvik123 <gadhiyahrithvik@gmail.com>
2023-03-07 22:37:03 -0700

    change how user dao is used by backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -62,7 +62,6 @@ app.use(express.urlencoded({extended: false}));
 );*/
 
 
-const udb = new UserDAO();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -202,25 +201,31 @@ app.post('/addFileServer', (req, res) => {
 
 
 // adds user to the userDB
-app.post('/registerUser', async (req,res) =>{
-    let user = req.body;
+    app.post('/registerUser', async (req,res) =>{
+        let user = req.body;
 
-    // generate userID
-    const dateStr:any = Date.now().toString(36); // convert num to base 36 and stringify
-    const randomStr:any = Math.random().toString(36).substring(2, 8); // start at index 2 to skip decimal point
-    const id = `${dateStr}-${randomStr}`;
-    user.id = id;
+        // generate userID
+        const dateStr:any = Date.now().toString(36); // convert num to base 36 and stringify
+        const randomStr:any = Math.random().toString(36).substring(2, 8); // start at index 2 to skip decimal point
+        const id = `${dateStr}-${randomStr}`;
+        user.id = id;
 
-    // hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedpw = await bcrypt.hash(user.password, salt);
-    user.password = hashedpw;
+        // hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedpw = await bcrypt.hash(user.password, salt);
+        user.password = hashedpw;
 
-    console.log(user);
-
-        udb.addUser(user, (rows: any) => {
-            res.status(200).json(rows);
-        });
+        console.log(user);
+        try {
+            const udb = new UserDAO();
+            udb.addUser(user, (rows: any) => {
+                res.status(200).json(rows);
+            });
+            udb.end();
+        } catch(ex) {
+            res.status(500).send({ error : "Could not upload file", message: ex || "Unknown"})
+            console.log('err: '+ ex || 'undefined');
+        }
     })
 
     function generateAccessToken(payload:any) {
@@ -231,47 +236,62 @@ app.post('/registerUser', async (req,res) =>{
         const refreshToken = req.body.token;
         const userID = req.body.userID;
         if (refreshToken == null) return res.sendStatus(401);
-        udb.getRefreshToken(userID, (rows: any) => {
-            if(rows[0].refreshToken == null) return res.sendStatus(403);
-            jwt.verify(refreshToken, 'refreshSecretKey', (err: any, user: any) => {
-                if(err) return res.sendStatus(403)
-                const accessToken = generateAccessToken({email: user.email})
-                res.json({accessToken: accessToken});
-            })    
-        })
+        try {
+            const udb = new UserDAO();
+            udb.getRefreshToken(userID, (rows: any) => {
+                if(rows[0].refreshToken == null) return res.sendStatus(403);
+                jwt.verify(refreshToken, 'refreshSecretKey', (err: any, user: any) => {
+                    if(err) return res.sendStatus(403)
+                    const accessToken = generateAccessToken({email: user.email})
+                    res.json({accessToken: accessToken});
+                })    
+            })
+            udb.end();
+        } catch(ex) {
+            res.status(500).send({ error : "Could not generate refresh token", message: ex || "Unknown"})
+            console.log('err: '+ ex || 'undefined');
+        }
+        
         // if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
     })
 
     // checks if user exists in the userDB
     app.post('/login', async (req,res) => {
         let user = req.body;
-        
-        udb.getUser(user.email, async (rows: any) => {
-            const result = rows[0];
-            if (!result) {
-                return res.status(404).json("user not found");
-            } else {
-                //check if password is valid
-                const validpw = await bcrypt.compare(user.password, result.password);
-                if (!validpw) {
-                    return res.status(400).json("invalid pw");
+        try {
+            const udb = new UserDAO();
+            udb.getUser(user.email, async (rows: any) => {
+                const result = rows[0];
+                if (!result) {
+                    return res.status(404).json("user not found");
                 } else {
-                    const payload = {email: user.email}
-                    const accessToken = generateAccessToken(payload);
-                    const refreshToken = jwt.sign(payload, 'refreshSecretKey');
-
-                    console.log("Refresh Token: "+refreshToken);
-                    console.log("UserID",result.id);
-                  
-                    files=[{userID:user.id}];
-                    // refreshTokens.push(refreshToken);
-                    console.log(files)
-                    udb.addRefreshToken(result.id, refreshToken, (rows: any)=> {
-                        return res.status(200).json({userID: result.id, accessToken: accessToken, refreshToken: refreshToken});
-                    })
+                    //check if password is valid
+                    const validpw = await bcrypt.compare(user.password, result.password);
+                    if (!validpw) {
+                        return res.status(400).json("invalid pw");
+                    } else {
+                        const payload = {email: user.email}
+                        const accessToken = generateAccessToken(payload);
+                        const refreshToken = jwt.sign(payload, 'refreshSecretKey');
+    
+                        console.log("Refresh Token: "+refreshToken);
+                        console.log("UserID",result.id);
+                      
+                        files=[{userID:user.id}];
+                        // refreshTokens.push(refreshToken);
+                        console.log(files)
+                        udb.addRefreshToken(result.id, refreshToken, (rows: any)=> {
+                            return res.status(200).json({userID: result.id, accessToken: accessToken, refreshToken: refreshToken});
+                        })
+                    }
                 }
-            }
-        });
+            });
+            udb.end();
+        } catch(ex) {
+            res.status(403).send({ error : "Could not login", message: ex || "Unknown"})
+            console.log('err: '+ ex || 'undefined');
+        }
+        
     })
 
 // // checks if user exists in the userDB
@@ -295,12 +315,16 @@ app.post('/registerUser', async (req,res) =>{
 
     app.delete('/logout', (req, res) => {
         // refreshTokens = refreshTokens.filter((token:any) => token !== req.body.token)
-        
-        udb.removeRefreshToken(req.body.id, (rows: any) => {
-            return res.status(204).json("logout successful, refresh token deleted");
-        })
-
-
+        try {
+            const udb = new UserDAO(); 
+            udb.removeRefreshToken(req.body.id, (rows: any) => {
+                return res.status(204).json("logout successful, refresh token deleted");
+            })
+            udb.end();
+        } catch(ex) {
+            res.status(500).send({ error : "Could not logout", message: ex || "Unknown"})
+            console.log('err: '+ ex || 'undefined');
+        }
     })
     
     // put below programs to other server (file managing) 
