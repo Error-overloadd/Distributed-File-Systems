d0918dc8daa26a351489d4263d7efd07b5c75ff7
Zixing Wei <60620430+Archer-zwei@users.noreply.github.com>
2023-03-08 01:12:41 -0700

    JWT frontend Done

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,14 +1,29 @@
-let resourcesServer =new axios.create({
+let generalRequest =new axios.create({
     baseURL:"http://localhost:3002/",
     crossDomain: true
 })
-let loginUser = true;
-
+let requestConfig = {
+    baseURL:"http://localhost:3002/",
+    crossDomain: true
+}
+let loginUser = false;
+const expiredTime = 3500;
 /*
 * ====================================================================
 * User Handling part
 * ====================================================================
 * */
+
+function addJWT(post){
+    const userID = localStorage.getItem('userID')
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken && userID){
+        post.headers.Authorization =  'Bearer'+' '+ accessToken;
+        post.data = post.data || new FormData();
+        post.data.append('userID',userID);
+    }
+    return post;
+}
 function userRegister(){
     let userName = document.querySelector("#username").value
     let passWord = document.querySelector("#pass").value;
@@ -18,11 +33,10 @@ function userRegister(){
         password:passWord,
         email:email,
         isAdmin:0
-
     }
     console.log(typeof(passWord),typeof(userName),typeof(email));
     console.log(JSON.stringify(datas));
-    resourcesServer({
+    generalRequest({
         method:'post',
         url:"registerUser/",
         headers:{
@@ -32,41 +46,24 @@ function userRegister(){
         // data:{id:12}
     }).then(res=>{
         if(res.status >= 200 && res.status < 300){
-            // console.log("It works rn")
-             console.log(res)
+            console.log("It works rn")
+            console.log(res)
         }
     }).catch(function (error){
         if (error.status === 404){
-
         }else{
             console.log(error.message)
         }
-
     })
-
-
 }
 
+
 function userLogin(){
-    // let userName = document.querySelector("#username").value
-    // let passWord = document.querySelector("#pass").value;
-    // resourcesServer({
-    //     method:'get',
-    //     url:"authenticateUser",
-    //     userName:userName,
-    //     passWord:passWord
-    // }).then(res=>{
-    //     if(res.status >= 200 && res.status < 300){
-    //         console.log(res.data.toString())
-    //         loginUser=userName
-    //     }
-    // }).catch(function (error){
-    //     console.log(error.message)
-    // })
+    let username = document.querySelector("#username").value;
     let password = document.querySelector("#pass").value;
     let email = document.querySelector("#email").value;
     console.log(email+"\n"+password);
-     resourcesServer({
+    generalRequest({
         method: 'post',
         url: "login",
         data:{
@@ -78,20 +75,54 @@ function userLogin(){
             const userID = res.data.userID;
             const accessToken = res.data.accessToken;
             const refreshToken = res.data.refreshToken;
-
+            const newTokenTime = new Date().getTime().toString();
             localStorage.setItem('userID', userID);
             localStorage.setItem('accessToken', accessToken);
             localStorage.setItem('refreshToken', refreshToken);
+            localStorage.setItem('tokenTime',newTokenTime);
             document.querySelector("#loginStatus").innerHTML="login success";
+            console.log("#############");
+            console.log("tokenTime");
+            console.log(newTokenTime);
+            console.log("user ID")
+            console.log(userID);
+            console.log("accessToken:")
+            console.log(accessToken);
+            console.log("refreshToken");
+            console.log(refreshToken);
+            console.log("#############");
             loginUser = true;
         }
     }).catch(function (error){
         document.querySelector("#loginStatus").innerHTML="login failed"+"<br>"+error.message
     })
 
-
 }
 
+function logout(){
+    if (localStorage.getItem("accessToken") !== null){;
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+        localStorage.removeItem('tokenTime');
+        localStorage.removeItem('userID');
+    }
+    document.querySelector("#loginStatus").innerHTML="Not Login yet";
+}
+
+function checkisLogin() {
+    const accessToken = localStorage.getItem('accessToken');
+    const tokenTime = localStorage.getItem('tokenTime');
+    const timeDiffInSec = (new Date().getTime() - tokenTime) / 1000;
+    if (accessToken) {
+        if (timeDiffInSec < expiredTime){
+            loginUser = true;
+            document.querySelector("#loginStatus").innerHTML="login success";
+        }else{
+            logout();
+        }
+
+    }
+}
 
 /*
 * ====================================================================
@@ -106,7 +137,7 @@ function userLogin(){
 
 function getFile(){
     let fileName = document.querySelector("#getFileName").value;
-    resourcesServer({
+    generalRequest({
         method:'GET',
         url:"getByFileName/",
         responseType: 'arraybuffer',
@@ -133,26 +164,37 @@ function getFile(){
 * can be done by login user only
 * */
 function uploadFile(){
+
     let file =document.querySelector("#uploadFile").files[0]
+    document.querySelector("#uploadFileStatus").innerHTML = "";
+    if (document.querySelector("#uploadFile").files.length === 0){
+        alert("please choose a file");
+        return;
+    }
     const formData = new FormData();
     formData.append('file', file);
-    console.log(file.length);
-    resourcesServer({
-        method:'post',
-        url:"addFile",
-        data:formData,
-        headers: {
-            'Content-Type': 'multipart/form-data'
-        }
-    }).then(res => {
-        if(res.status >= 200 && res.status < 300){
-            document.querySelector("#uploadFileStatus").innerHTML=res.data;
-        }
-    }).catch(error => {
-        console.log(error);
-    });
-}
+    updateToken();
+    if (loginUser){
+        const newRequest = axios.create(requestConfig);
+        newRequest.interceptors.request.use(addJWT);
+        newRequest.post('addFile',formData,{
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then(res => {
+            if(res.status >= 200 && res.status < 300){
+                document.querySelector("#uploadFileStatus").innerHTML= res.data;
+            }else{
+                document.querySelector("#uploadFileStatus").innerHTML="error:"+ res.data;
+            }
+        }).catch(error => {
+            console.log(error);
+        });
+    }else{
+        alert("please login");
+    }
 
+}
 /*
 * request for delete file on server
 * can be done by login user only
@@ -160,16 +202,18 @@ function uploadFile(){
 
 function deleteFile(){
     let fileName =document.querySelector("#deleteFileName").value;
-    //const message =  document.querySelector('.chat_box').value;
+    document.querySelector("#deleteFileStatus").innerHTML = "";
     if(loginUser){
-        resourcesServer({
-            method:'delete',
-            url:"deleteByFilename",
+        updateToken();
+        const newRequest = axios.create(requestConfig);
+        newRequest.interceptors.request.use(addJWT);
+        newRequest.delete('deleteByFilename',{
             params: {
                 fileName:fileName
             }
         }).then(res=>{
             if(res.status >= 200 && res.status < 300){
+                console.log("file deleted");
                 document.querySelector("#deleteFileStatus").innerHTML=res.data;
             }
         }).catch(function (error){
@@ -186,27 +230,18 @@ async function fetchFiles(){
     if (loginUser == false){
         alert("please login");
     }else {
-        //await updateToken();
-        const userID = localStorage.getItem('userID');
-        const token = localStorage.getItem('accessToken');
-        console.log("This is test:",userID);
-        console.log("This is test:",token);
-       
-        resourcesServer({
-            // method: 'post',
-            // url: "fetchFiles",
-            // headers:{Authorization: `Bearer ${token}`}
-            method: 'POST',
+        updateToken();
+        const newRequest = axios.create(requestConfig);
+        newRequest.interceptors.request.use(addJWT);
+        newRequest({
+            method: 'get',
             url: "fetchFiles",
-            headers:{Authorization: `Bearer ${token}` }
-            
         }).then(res => {
             if (res.status >= 200 && res.status < 300) {
                 console.log("fetchFiles succeed! \n"+res.data);
             }
         }).catch(function (error) {
             console.log("fetchFiles failed! \n"+error.message);
-           // console.log(headers);
         })
     }
 }
@@ -218,23 +253,33 @@ async function fetchFiles(){
 function updateToken(){
     const refreshToken = localStorage.getItem('refreshToken');
     const userID = localStorage.getItem('userID');
-    resourcesServer({
-        method: 'post',
-        url: "token",
-        data:{
-            userID:userID,
-            token:refreshToken
-        }
-    }).then(res=>{
-        if(res.status === 200){
-            const accessToken = res.data.accessToken;
-            localStorage.setItem('accessToken', accessToken);
-            // console.log("Updated access Token");
-            // console.log(accessToken);
-            // console.log("#############");
-        }
-    }).catch(function (error){
-        alert("Token failed, please try login again"+"\n"+error.message);
-    })
-
+    const tokenTime = localStorage.getItem('tokenTime');
+    const timeDiffInSec = (new Date().getTime() - tokenTime)/1000;
+    if (timeDiffInSec > expiredTime){
+        generalRequest({
+            method: 'post',
+            url: "token",
+            data:{
+                userID:userID,
+                token:refreshToken
+            }
+        }).then(res=>{
+            if(res.status === 200){
+                const accessToken = res.data.accessToken;
+                const newTokenTime = new Date().getTime().toString();
+                localStorage.setItem('accessToken', accessToken);
+                localStorage.setItem('tokenTime',newTokenTime);
+                console.log("Updated access Token");
+                console.log(accessToken);
+                console.log(newTokenTime);
+                console.log("#############");
+            }
+        }).catch(function (error){
+            alert("Token failed, please try login again"+"\n"+error.message);
+        })
+    }else{
+        console.log("token not expired yet");
+    }
 }
+checkisLogin();
+
