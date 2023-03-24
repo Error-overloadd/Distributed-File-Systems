987faed2de4fcaecea4d1524ce90220d1ef8271d
hrithvik123 <gadhiyahrithvik@gmail.com>
2023-03-24 16:33:09 -0600

    sync files across fileserver

--- fileserver/src/rabbitmq/broker.ts
+++ fileserver/src/rabbitmq/broker.ts
@@ -4,6 +4,7 @@ import { NAME } from '..';
 import { STORAGE_PATH } from '../constants';
 var path = require("path");
 import * as fs from "fs";
+import axios from 'axios';
 
 var channel: Channel, connection: Connection;
 const RabbitMQ_ADDR = "amqp://rabbitmq:5672";
@@ -24,7 +25,7 @@ export async function connectQueue() {
 		channel.consume(queue, (data: any) => {
 			// console.log(data)
 			console.log("Data received : ", JSON.parse(`${Buffer.from(data.content)}`));
-			//handleMessage(JSON.parse(`${Buffer.from(data.content)}`));
+			handleMessage(JSON.parse(`${Buffer.from(data.content)}`));
 			channel.ack(data);
 		});
 	} catch (error) {
@@ -47,52 +48,66 @@ export const sendMessage = async (data: any) => {
 	// await connection.close();
 };
 
-// //{task: "NewFile", id: rows.insertId, fileObj, address: `http://${ACCESS_URL}`}
-// const handleMessage = async (msg: any) => {
-// 	if(msg.task === "NewFile") {
-// 		downloadFile(msg.address, path.join(STORAGE_PATH, msg.fileObj.name));
-// 	};
-// 	if(msg.task === 'DeleteFile'){
-// 		deleteFile(path.join(STORAGE_PATH, msg.fileObj.name));
-// 	}
-// };
+//{task: "NewFile", id: rows.insertId, fileObj, address: `http://${ACCESS_URL}`}
+const handleMessage = async (msg: any) => {
+	if(msg.source == NAME) {
+		return;
+	}
+	if(msg.task === "NewFile") {
+		downloadFile(msg.address, msg);
+	};
+	if(msg.task === 'DeleteFile'){
+		deleteFile(path.join(STORAGE_PATH, msg.fileObj.name));
+	}
+};
 
 
-// async function downloadFile(fileUrl: string, outputLocationPath: string) {
-// 	const writer = createWriteStream(outputLocationPath);
+async function downloadFile(fileUrl: string, msg: any) {
+	try {
+		fs.mkdirSync(STORAGE_PATH, { recursive: true });
+		let outputLocationPath = path.join(STORAGE_PATH, msg.fileObj.name);
+		const writer = createWriteStream(outputLocationPath);
   
-// 	axios({
-// 		method: 'get',
-// 		url: fileUrl,
-// 		responseType: 'stream',
-// 	}).then(response => {
+		axios({
+			method: 'get',
+			url: fileUrl,
+			responseType: 'stream',
+		}).then(response => {
   
-// 	  //ensure that the user can call `then()` only when the file has
-// 	  //been downloaded entirely.
+	  //ensure that the user can call `then()` only when the file has
+	  //been downloaded entirely.
   
-// 	  return new Promise((resolve, reject) => {
-// 		response.data.pipe(writer);
-// 		let error: Error | null = null;
-// 		writer.on('error', err => {
-// 		  error = err;
-// 		  writer.close();
-// 		  reject(err);
-// 		});
-// 		writer.on('close', () => {
-// 		  if (!error) {
-// 			console.log("MQ saved file: ", outputLocationPath);
-// 			resolve(true);
-// 		  }
-// 		  //no need to call the reject here, as it will have been called in the
-// 		  //'error' stream;
-// 		});
-// 	  });
-// 	});
-//   }
+		return new Promise((resolve, reject) => {
+			response.data.pipe(writer);
+			let error: Error | null = null;
+			writer.on('error', err => {
+			error = err;
+			writer.close();
+			reject(err);
+			});
+			writer.on('close', () => {
+			if (!error) {
+				console.log(NAME+ " saved file: ", outputLocationPath);
+				resolve(true);
+			}
+			//no need to call the reject here, as it will have been called in the
+			//'error' stream;
+				});
+			});
+		});
+	} catch(ex) {
+		console.log("exception: ", ex);
+	}
+	
+  }
 
-//   const deleteFile = async (filePath: string) => {
-// 	fs.unlink(filePath, (err) => {
-// 		console.log("Couldn't delete file: " +filePath);
-// 		console.log("MQ del err: " +err);
-// 	});
-//   }
+  const deleteFile = async (filePath: string) => {
+	fs.unlink(filePath, (err) => {
+		if(err) {
+			console.log("Couldn't delete file: " +filePath);
+			console.log("MQ del err: " +err);
+		} else {
+			console.log(NAME +" deleted " +filePath);
+		}
+	});
+  }
