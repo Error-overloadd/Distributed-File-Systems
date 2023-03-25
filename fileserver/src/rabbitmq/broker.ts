4993261bba201e0be6d63604c1016bf7d2907ae6
hrithvik123 <gadhiyahrithvik@gmail.com>
2023-03-25 15:19:33 -0600

    make fileserver queues durable for synchronization

--- fileserver/src/rabbitmq/broker.ts
+++ fileserver/src/rabbitmq/broker.ts
@@ -12,7 +12,7 @@ const FILE_EXCHANGE = "fileExchange";
 const EXCHANGE_TYPE = "fanout";
 const ROUTING_KEY = "";
 const OPTIONS = {};
-const QUEUE = NAME;
+const QUEUE: string | undefined = process.env.NAME;
 
 export async function connectQueue() {
 	try {
@@ -20,13 +20,15 @@ export async function connectQueue() {
 		channel = await connection.createChannel();
 		await channel.assertExchange(FILE_EXCHANGE, EXCHANGE_TYPE, OPTIONS);
 		// connect to 'test-queue', create one if doesnot exist already
-		const { queue } = await channel.assertQueue(QUEUE, {exclusive: true});
+		const { queue } = await channel.assertQueue(`${QUEUE}`);
 		await channel.bindQueue(queue, FILE_EXCHANGE, ROUTING_KEY);
-		channel.consume(queue, (data: any) => {
+		channel.consume(queue, async (data: any) => {
 			// console.log(data)
-			console.log("Data received : ", JSON.parse(`${Buffer.from(data.content)}`));
-			handleMessage(JSON.parse(`${Buffer.from(data.content)}`));
-			channel.ack(data);
+			console.log("Message received : ", JSON.parse(`${Buffer.from(data.content)}`));
+			const success = await handleMessage(JSON.parse(`${Buffer.from(data.content)}`));
+			if(success) {
+				channel.ack(data);
+			}
 		});
 	} catch (error) {
 		console.log(error);
@@ -36,12 +38,17 @@ export async function connectQueue() {
 export const sendMessage = async (data: any) => {
 	// send data to queue
 	// await channel.sendToQueue(FILE_QUEUE, Buffer.from(JSON.stringify(data)));
-	channel.publish(
-		FILE_EXCHANGE,
-		ROUTING_KEY,
-		Buffer.from(JSON.stringify(data)),
-		OPTIONS
-	);
+	try{
+		channel.publish(
+			FILE_EXCHANGE,
+			ROUTING_KEY,
+			Buffer.from(JSON.stringify(data)),
+			OPTIONS
+		);
+	} catch (ex) {
+		console.log(NAME +" could not send message to exchange");
+		console.log(NAME +" : " +ex);
+	}
 
 	// close the channel and connection
 	// await channel.close();
@@ -49,41 +56,47 @@ export const sendMessage = async (data: any) => {
 };
 
 //{task: "NewFile", id: rows.insertId, fileObj, address: `http://${ACCESS_URL}`}
-const handleMessage = async (msg: any) => {
+const handleMessage = async (msg: any): Promise<boolean> => {
 	if(msg.source == NAME) {
-		return;
+		return true;
 	}
 	if(msg.task === "NewFile") {
-		downloadFile(msg.address, msg);
+		const success = await downloadFile(msg.address, msg);
+		return success;
 	};
 	if(msg.task === 'DeleteFile'){
-		deleteFile(path.join(STORAGE_PATH, msg.fileObj.name));
+		try {
+			await deleteFile(path.join(STORAGE_PATH, msg.fileObj.name));
+			return true;
+		} catch (ex) {
+			return false;
+		}
 	}
+	return true;
 };
 
 
-async function downloadFile(fileUrl: string, msg: any) {
+async function downloadFile(fileUrl: string, msg: any): Promise<boolean> {
 	try {
 		fs.mkdirSync(STORAGE_PATH, { recursive: true });
 		let outputLocationPath = path.join(STORAGE_PATH, msg.fileObj.name);
 		const writer = createWriteStream(outputLocationPath);
   
-		axios({
+		const response = await axios({
 			method: 'get',
 			url: fileUrl,
 			responseType: 'stream',
-		}).then(response => {
-  
-	  //ensure that the user can call `then()` only when the file has
-	  //been downloaded entirely.
+		});
+		//ensure that the user can call `then()` only when the file has
+	  	//been downloaded entirely.
   
 		return new Promise((resolve, reject) => {
 			response.data.pipe(writer);
 			let error: Error | null = null;
 			writer.on('error', err => {
-			error = err;
-			writer.close();
-			reject(err);
+				error = err;
+				writer.close();
+				reject(false);
 			});
 			writer.on('close', () => {
 			if (!error) {
@@ -94,9 +107,9 @@ async function downloadFile(fileUrl: string, msg: any) {
 			//'error' stream;
 				});
 			});
-		});
 	} catch(ex) {
 		console.log("exception: ", ex);
+		return false;
 	}
 	
   }
@@ -106,6 +119,7 @@ async function downloadFile(fileUrl: string, msg: any) {
 		if(err) {
 			console.log("Couldn't delete file: " +filePath);
 			console.log("MQ del err: " +err);
+			throw err;
 		} else {
 			console.log(NAME +" deleted " +filePath);
 		}
